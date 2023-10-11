bd9924fe7fea98484eed608a7ef9fc16cd42dddd
ahmedwahba333 <ahmedmismail39@gmail.com>
2023-10-12 00:22:35 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+createApp(App).use(store).use(router).mount("#app");
