6a1690cb454a894914bb2092511cf0bbe9388a17
SaraAliNagy <saranagy47@gmail.com>
2023-10-13 03:53:11 +0300

    dropDownNav

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 <template>
   <!-- <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> -->
-  <NavBarPages/>
-  <LandingPage/>
+  <NavBarPages />
+  <LandingPage />
   <router-view />
 </template>
 <script>
@@ -12,29 +12,29 @@ import LandingPage from "@/views/LandingPage.vue";
 export default {
   components: {
     NavBarPages,
-    LandingPage
+    LandingPage,
   },
 };
 </script>
 <style lang="scss">
-// #app {
-//   font-family: Raleway, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-// nav {
-//   padding: 30px;
+nav {
+  padding: 30px;
 
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
--- src/components/NavBarPages.vue
+++ src/components/NavBarPages.vue
@@ -8,6 +8,33 @@
         </li>
         <li>
           <router-link to="#Services"><a>Services</a></router-link>
+          <div class="mega-menu">
+            <ul class="links">
+              <ul><span>House Keeping</span>
+                <li><router-link to="#HouseKeeping"><a>Cleaning</a></router-link></li>
+                <li><router-link to="#HouseKeeping"><a>Gardening</a></router-link></li>
+              </ul>
+              <ul><span>Giving</span>
+                <li><router-link to="#CareGiving"><a>Baby sitting</a></router-link></li>
+                <li><router-link to="#CareGiving"><a>Elderly care</a></router-link></li>
+                <li><router-link to="#CareGiving"><a>Home nursing</a></router-link></li>
+
+              </ul>
+              <ul><span>Food Services</span>
+                <li><router-link to="#FoodServices"><a>Food Catering</a></router-link></li>
+                <li><router-link to="#FoodServices"><a>Chefs</a></router-link></li>
+              </ul>
+              <ul><span>House Maintenance</span>
+                <li><router-link to="#HouseMaintenance"><a>Plumbing</a></router-link></li>
+                <li><router-link to="#HouseMaintenance"><a>Carpentry</a></router-link></li>
+                <li><router-link to="#HouseMaintenance"><a>Moving Furniture</a></router-link></li>
+              </ul>
+              <ul><span>Facilities</span>
+                <li><router-link to="#Facilities"><a>House Guard</a></router-link></li>
+                <li><router-link to="#Facilities"><a>Driver</a></router-link></li>
+              </ul>
+            </ul>
+          </div>
         </li>
         <li>
           <router-link to="#Workers"><a>Workers</a></router-link>
@@ -31,120 +58,194 @@ export default {
 
 <style lang="scss" scoped>
 @import "../scss/global/colors";
+
+$blueColor: #193655 ; 
+$orangeColor : #F97B22 ; 
+$darkOrange: #C15000 ; 
+$backgroudColor : #EBEBD3  ;
+$whiteColor : #FFFFFF;
 a {
-  text-decoration: none;
+	text-decoration: none;
 }
 ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
+	list-style: none;
+	margin: 0;
+	padding: 0;
 }
 .container {
-  padding-left: 15px;
-  padding-right: 15px;
-  margin-left: auto;
-  margin-right: auto;
+	padding-left: 15px;
+	padding-right: 15px;
+	margin-left: auto;
+	margin-right: auto;
 }
+
 .header {
-  background-color: $blueColor;
-  position: relative;
-  display: flex;
-  flex-wrap: wrap;
-  .container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-wrap: wrap;
-    position: relative;
-  }
+	background-color: $blueColor;
+	position: relative;
+	.container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		flex-wrap: wrap;
+		position: relative;
+	}
   .logo {
-    font-size: 30px;
-    color: $whiteColor;;
-    font-weight: bold;
-    height: 73.56px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .mainNav {
-    display: flex;
-    > li {
-      > a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 73.56px;
-        position: relative;
-        color: $whiteColor;;
-        padding: 0 30px;
-        transition: var(--main-transition);
-        overflow: hidden;
-        &::before {
-          content: "";
-          position: absolute;
-          width: 100%;
-          height: 4px;
-          background-color: $orangeColor;
-          top: 0;
-          left: -100%;
-          transition: var(--main-transition);
-        }
-        &:hover {
-          color: $blueColor;
-          background-color: $backgroudColor;
-          &::before {
-            left: 0;
-          }
-        }
+		color: $whiteColor;
+		font-size: 26px;
+		font-weight: bold;
+		height: 72px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.mainNav{
+		display: flex;
+		>li {
+			&:hover {
+				.mega-menu {
+					opacity: 1;
+					z-index: 100;
+					top: calc(100% + 1px);
+				}
+			}
+			>a {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				height: 72px;
+				position: relative;
+				color: $whiteColor;
+				padding: 0 30px;
+				overflow: hidden;
+				font-size: 24px;
+				transition: var(--main-transition);
+				&::before {
+					content: "";
+					position: absolute;
+					width: 100%;
+					height: 4px;
+					background-color: $orangeColor;
+					top: 0;
+					left: -100%;
+					transition: var(--main-transition);
+				}
+				&:hover {
+					color: $blueColor;
+					background-color: $backgroudColor ;
+					&::before {
+						left: 0;
+					}
+				}
+			}
+		}
+	}
+	.mega-menu {
+		position: absolute;
+		width: 20%;
+		left: 50;
+		padding: 15px;
+		background-color: $backgroudColor ;
+		border-bottom: 3px solid $blueColor;
+		z-index: -1;
+		display: flex;
+		gap: 15px;
+		top: calc(100% + 50px);
+		opacity: 0;
+		transition: top var(--main-transition), opacity var(--main-transition);
+
+		.links {
+			min-width: 100px;
+			flex: 1;
+      span{
+        color: $blueColor;
+					padding: 2px;
+					display: block;
+					font-size: 18px;
+					font-weight:bold;
+          text-align: left;
+          border-bottom: 1px solid $orangeColor;
       }
-    }
-  }
+			li {
+				position: relative;
+				&:not(:last-child) {
+					// border-bottom: 1px solid $blueColor;
+				}
+				&::before {
+					content: "";
+					position: absolute;
+					left: 0;
+					top: 0;
+					width: 0;
+					height: 100%;
+					background-color: $whiteColor;
+					z-index: -1;
+					transition: var(--main-transition);
+				}
+				&:hover {
+					&::before {
+						width: 100%;
+					}
+				}
+       
+				a {
+					color: $blueColor;
+					padding: 2px;
+					display: block;
+					font-size: 14px;
+					font-weight:bold;
+          text-align: left;
+				}
+			}
+		}
+	}
 }
 @media (min-width: 768px) {
-  .container {
-    width: 750px;
-  }
+	.container {
+		width: 750px;
+	}
 }
 @media (min-width: 992px) {
-  .container {
-    width: 970px;
-  }
+	.container {
+		width: 970px;
+	}
 }
 @media (min-width: 1200px) {
-  .container {
-    width: 1170px;
-  }
+	.container {
+		width: 1170px;
+	}
 }
 @media (max-width: 767px) {
-  .header {
-    .logo {
-      width: 100%;
-      height: 50px;
-    }
-    .mainNav {
-      margin: auto;
-      > li {
-        > a {
-          padding: 10px;
-          font-size: 14px;
-          height: 30px;
-        }
-      }
-    }
-  }
+	.header {
+		.logo {
+			width: 100%;
+			height: 50px;
+		}
+		.main-nav {
+			margin: auto;
+			>li {
+				>a {
+					padding: 10px;
+					font-size: 14px;
+					height: 40px;
+				}
+			}
+		}
+		.mega-menu {
+			flex-direction: column;
+			gap: 0;
+			padding: 5px;
+			.links {
+				&:first-of-type {
+					li {
+						&:last-child {
+							border-bottom: 1px solid $backgroudColor ;
+						}
+					}
+				}
+			}
+		}
+	}
 }
-@media (max-width: 375px) {
-  .header {
-    .mainNav {
-      margin: auto;
-      > li {
-        > a {
-          padding: 15px;
-          font-size: 10px;
-          height: 30px;
-        }
-      }
-    }
-  }
-}
+
 </style>
