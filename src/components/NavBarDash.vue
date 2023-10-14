4a8aa4eda2a4d3c50c2350826942ff18cb7f14dc
SaraAliNagy <saranagy47@gmail.com>
2023-10-14 03:52:53 +0300

    navbarDash

--- src/components/NavBarDash.vue
+++ src/components/NavBarDash.vue
@@ -1,34 +1,16 @@
 <template>
-  <!--Navbar-->
-<nav class="navbar navbar-expand-lg navbar-dark indigo mb-4">
 
-  <!-- Navbar brand -->
-  <a class="navbar-brand" href="#">Navbar</a>
-
-  <!-- Collapsible content -->
-  <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
-    <!-- <form class="form-inline d-flex justify-content-center md-form form-sm mt-0">
-      <input class="form-control form-control-sm ml-3 w-75" type="text"  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" >
-        <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
-        
-        aria-label="Search">
-    </form>
-    <form class="form-inline ml-auto">
-      <div class="md-form my-0">
-        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" >
-        <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
-        <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-      </div>
-      
-    </form> -->
-
-<!-- html code -->
-<input type="text" name="txtBox" >
-  <!-- </div> -->
-  <!-- Collapsible content -->
-
-</nav>
-
+<div class="header" id="header">
+    <div class="container">
+    <router-link to="#" class="logo"><a>Awon</a></router-link>
+   
+    
+      <form class="search">
+        <input class="search" type="search" />
+      </form>
+            <img src="../assets/images/2.jpg" alt="Cx" />
+    </div>
+</div>
 </template>
 
 <script>
@@ -39,41 +21,73 @@ export default {
 
 <style lang="scss" scoped>
 @import "../scss/global/colors";
-input {
-  background: url("http://kodyrabatowe.wp.pl/img/ico/search_gr.png") top left
-    no-repeat;
-  height: 30px;
-  padding-left: 25px;
+.header {
+  background-color: $blueColor;
+  position: relative;
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    position: relative;
+  }
+  .logo {
+    color: $whiteColor;
+    font-size: 26px;
+    font-weight: bold;
+    height: 72px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  img {
+    clip-path: circle();
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  & input.search {
+  border: 2px solid $orangeColor;
+  border-radius: 28px;
+  width: 350px;
+  padding: 9px 4px 9px 40px;
+  background: $backgroudColor
+    url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='orange' width='16' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E")
+    no-repeat 13px center;
+  &:focus {
+    border-color: $orangeColor;
+    outline: none;
+  }
 }
-// a {
-// 	text-decoration: none;
-// }
+}
 
-// .container {
-// 	padding-left: 15px;
-// 	padding-right: 15px;
-// 	margin-left: auto;
-// 	margin-right: auto;
-// }
+a {
+  text-decoration: none;
+}
 
-// .header {
-// 	background-color: $blueColor;
-// 	position: relative;
-// 	.container {
-// 		display: flex;
-// 		justify-content: space-between;
-// 		align-items: center;
-// 		flex-wrap: wrap;
-// 		position: relative;
-// 	}
-//   .logo {
-// 		color: $whiteColor;
-// 		font-size: 26px;
-// 		font-weight: bold;
-// 		height: 72px;
-// 		display: flex;
-// 		justify-content: center;
-// 		align-items: center;
-// 	}
-// }
+@media (min-width: 768px) {
+  .container {
+    width: 750px;
+  }
+}
+@media (min-width: 992px) {
+  .container {
+    width: 970px;
+  }
+}
+@media (min-width: 1200px) {
+  .container {
+    width: 1170px;
+  }
+}
+@media (max-width: 767px) {
+  .header {
+    .logo {
+      width: 100%;
+      height: 50px;
+    }
+  }
+  }
+
 </style>
