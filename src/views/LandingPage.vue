6b4b2b4d3a25a4a18e47db7a7d79cc5cd8af1b5d
SaraAliNagy <saranagy47@gmail.com>
2023-10-14 06:08:18 +0300

    sliderLandingPage

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,5 +1,25 @@
 <template>
- <h1>handy hands that are happy to help</h1>
+ <!-- <h1>handy hands that are happy to help</h1> -->
+ <div id="myCarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000" >
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../assets/images/BGslider/pic1.png" alt="slide1">
+    </div>
+    <div class="carousel-item ">
+      <img class="d-block w-100" src="../assets/images/BGslider/pic2.jpeg" alt="slide2">
+    </div>
+    <div class="carousel-item ">
+      <img class="d-block w-100" src="../assets/images/BGslider/pic3.png" alt="slide3">
+    </div>
+    <div class="carousel-item ">
+      <img class="d-block w-100" src="../assets/images/BGslider/pic4.jpeg" alt="slide4">
+    </div>
+    <div class="carousel-item ">
+      <img class="d-block w-100" src="../assets/images/BGslider/pic5.png" alt="slide5">
+    </div>
+  </div>
+</div>
+
 </template>
 
 <script>
@@ -8,7 +28,9 @@ export default {
   name: 'LandingPage',
 
 }
+
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+
 </style>
