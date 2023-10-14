d24b2c638bd063156a9452acf1b1c41e4f397c52
amrLmohamed <ren84246@gmail.com>
2023-10-14 15:02:37 +0300

    change app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <template>
   <!-- <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> -->
-<<<<<<< HEAD
-  <!-- <NavBarPages/> -->
-   <!-- <LandingPage/> -->
-  <!-- <NavBarDash/> -->
-    <SideBarPage/>
-  <!-- <router-view/> -->
-=======
   <!-- <NavBarPages/>
    <LandingPage/> -->
   <!-- <NavBarDash/> -->
  
   <router-view/>
->>>>>>> 4aec41d466a14dd585df2238e4f543a5bb5d5b1a
   
     
   <!-- <FooterComponent/> -->
@@ -27,21 +19,13 @@
 // import NavBarDash from "@/components/NavBarDash.vue";
 // import LandingPage from "@/views/LandingPage.vue";
 // import FooterComponent from './components/footer.vue';
-<<<<<<< HEAD
-import SideBarPage from "./components/SideBarPage.vue";
-=======
->>>>>>> 4aec41d466a14dd585df2238e4f543a5bb5d5b1a
 export default {
   components: {
     // NavBarPages,
     // LandingPage,
     // NavBarDash,
-<<<<<<< HEAD
     // FooterComponent,
     SideBarPage
-=======
-    // FooterComponent
->>>>>>> 4aec41d466a14dd585df2238e4f543a5bb5d5b1a
   },
 };
 
