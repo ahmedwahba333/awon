134a99e633fc4107c994bc7b6663e68380dfc5ec
NadaA-Ibrahim <nada.a.ibrahim65@gmail.com>
2023-10-14 16:49:55 +0300

    added worker prof

---
+++ src/views/WorkerProfileCx.vue
@@ -0,0 +1,28 @@
+    <template>
+    <div class="bg">
+        <NavBarPages />
+        <h1>worker</h1>
+        <FooterComponent />
+    </div>
+    </template>
+
+    <script>
+    import NavBarPages from "@/components/NavBarPages.vue";
+    import FooterComponent from "@/components/footer.vue";
+
+    export default {
+    name: "WorkerProfile",
+    components: {
+        FooterComponent,
+        NavBarPages,
+    },
+    };
+    </script>
+
+    <style lang="scss" scoped>
+    @import "../scss/global/colors";
+
+    .bg{
+    background-color: red;
+    }
+    </style>
