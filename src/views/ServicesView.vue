5886d3b312869674d760a82731ce0ea19e6e0152
omar al farouk <omarelfarouk.tarek@gmail.com>
2023-10-15 22:00:27 +0300

    images

---
+++ src/views/ServicesView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <NavBarPages />
+    <div>
+      <h2 class="serv">House Keeping: Your home, your haven.</h2>
+      <p>
+        Your home is your sanctuary, a place where you can relax and recharge.
+        it can be tough to find the time to keep your home as clean and tidy as
+        you'd like with busy schedules. That's where housekeeping services come
+        in, so you can focus on what's important to you.
+      </p>
+    </div>
+    <!-- <footer/> -->
+  </div>
+</template>
+
+<script>
+import NavBarPages from "@/components/NavBarPages.vue";
+// import footer from '@/components/footer.vue';
+export default {
+  components: {
+    NavBarPages,
+    // footer
+  },
+};
+</script>
+
+<style scoped></style>
