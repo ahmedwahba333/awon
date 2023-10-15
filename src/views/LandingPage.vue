f6518b0ca491f798df933c16eca15297a7a1a015
SaraAliNagy <saranagy47@gmail.com>
2023-10-15 20:18:28 +0300

    Form

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -138,9 +138,11 @@
       <div class="row text-center my-5">
         <h2>Our Happy Customer</h2>
       </div>
-      <div class="row ">
-        <div class="col-12 d-flex flex-wrap flex-md-nowrap justify-content-around ">
-          <div class="box m-3 ">
+      <div class="row">
+        <div
+          class="col-12 d-flex flex-wrap flex-md-nowrap justify-content-around"
+        >
+          <div class="box">
             <div class="d-flex flex-row justify-content-between">
               <div class="p-2">
                 <h4>Fatma O.</h4>
@@ -184,7 +186,7 @@
               </p>
             </div>
           </div>
-         <div class="box m-3 ">
+          <div class="box">
             <div class="d-flex flex-row justify-content-between">
               <div class="p-2">
                 <h4>Ahmed E.</h4>
@@ -223,20 +225,42 @@
             </div>
             <div>
               <p class="p-2">
-                heard about Aown from a friend and gave them a try. very professional indeed.
+                heard about Aown from a friend and gave them a try. very
+                professional indeed.
               </p>
             </div>
           </div>
-          <div class="box m-3 ">
+          <div class="box">
             <div class="d-flex flex-row justify-content-between">
               <div class="p-2">
                 <h4>Eyad K.</h4>
-               <svg xmlns="http://www.w3.org/2000/svg" width="125" height="22" viewBox="0 0 125 22" fill="none">
-                  <path d="M10.8425 0.0842438L13.2768 7.57622L21.1543 7.57622L14.7813 12.2065L17.2155 19.6985L10.8425 15.0682L4.46943 19.6985L6.90373 12.2065L0.530669 7.57622L8.4082 7.57622L10.8425 0.0842438Z" fill="#F97B22"/>
-                  <path d="M36.5935 0.0842438L39.0278 7.57622L46.9053 7.57622L40.5322 12.2065L42.9665 19.6985L36.5935 15.0682L30.2204 19.6985L32.6547 12.2065L26.2816 7.57622L34.1592 7.57622L36.5935 0.0842438Z" fill="#F97B22"/>
-                  <path d="M62.3444 0.0842438L64.7787 7.57622L72.6563 7.57622L66.2832 12.2065L68.7175 19.6985L62.3444 15.0682L55.9714 19.6985L58.4057 12.2065L52.0326 7.57622L59.9102 7.57622L62.3444 0.0842438Z" fill="#F97B22"/>
-                  <path d="M88.0954 0.0842438L90.5297 7.57622L98.4072 7.57622L92.0342 12.2065L94.4685 19.6985L88.0954 15.0682L81.7224 19.6985L84.1567 12.2065L77.7836 7.57622L85.6611 7.57622L88.0954 0.0842438Z" fill="#F97B22"/>
-                  <path d="M113.846 0.0842438L116.281 7.57622L124.158 7.57622L117.785 12.2065L120.219 19.6985L113.846 15.0682L107.473 19.6985L109.908 12.2065L103.535 7.57622L111.412 7.57622L113.846 0.0842438Z" fill="#F97B22"/>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="125"
+                  height="22"
+                  viewBox="0 0 125 22"
+                  fill="none"
+                >
+                  <path
+                    d="M10.8425 0.0842438L13.2768 7.57622L21.1543 7.57622L14.7813 12.2065L17.2155 19.6985L10.8425 15.0682L4.46943 19.6985L6.90373 12.2065L0.530669 7.57622L8.4082 7.57622L10.8425 0.0842438Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M36.5935 0.0842438L39.0278 7.57622L46.9053 7.57622L40.5322 12.2065L42.9665 19.6985L36.5935 15.0682L30.2204 19.6985L32.6547 12.2065L26.2816 7.57622L34.1592 7.57622L36.5935 0.0842438Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M62.3444 0.0842438L64.7787 7.57622L72.6563 7.57622L66.2832 12.2065L68.7175 19.6985L62.3444 15.0682L55.9714 19.6985L58.4057 12.2065L52.0326 7.57622L59.9102 7.57622L62.3444 0.0842438Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M88.0954 0.0842438L90.5297 7.57622L98.4072 7.57622L92.0342 12.2065L94.4685 19.6985L88.0954 15.0682L81.7224 19.6985L84.1567 12.2065L77.7836 7.57622L85.6611 7.57622L88.0954 0.0842438Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M113.846 0.0842438L116.281 7.57622L124.158 7.57622L117.785 12.2065L120.219 19.6985L113.846 15.0682L107.473 19.6985L109.908 12.2065L103.535 7.57622L111.412 7.57622L113.846 0.0842438Z"
+                    fill="#F97B22"
+                  />
                 </svg>
               </div>
               <div>
@@ -245,25 +269,49 @@
             </div>
             <div>
               <p class="p-2">
-               I tried the services while travelling and I loved coming back to a clean house.
+                I tried the services while travelling and I loved coming back to
+                a clean house.
               </p>
             </div>
           </div>
         </div>
       </div>
-      <div class="row ">
-        <div class="col-12 d-flex flex-wrap flex-md-nowrap justify-content-around ">
-          <div class="box m-3 ">
+      <div class="row">
+        <div
+          class="col-12 d-flex flex-wrap flex-md-nowrap justify-content-around"
+        >
+          <div class="box">
             <div class="d-flex flex-row justify-content-between">
               <div class="p-2">
                 <h4>Sarah M.</h4>
-               <svg xmlns="http://www.w3.org/2000/svg" width="125" height="22" viewBox="0 0 125 22" fill="none">
-                    <path d="M10.8425 0.0842285L13.2768 7.57621L21.1543 7.57621L14.7813 12.2065L17.2155 19.6985L10.8425 15.0682L4.46943 19.6985L6.90373 12.2065L0.530669 7.57621L8.4082 7.57621L10.8425 0.0842285Z" fill="#F97B22"/>
-                    <path d="M36.5935 0.0842285L39.0278 7.57621L46.9053 7.57621L40.5322 12.2065L42.9665 19.6985L36.5935 15.0682L30.2204 19.6985L32.6547 12.2065L26.2816 7.57621L34.1592 7.57621L36.5935 0.0842285Z" fill="#F97B22"/>
-                    <path d="M62.3444 0.0842285L64.7787 7.57621L72.6563 7.57621L66.2832 12.2065L68.7175 19.6985L62.3444 15.0682L55.9714 19.6985L58.4057 12.2065L52.0326 7.57621L59.9102 7.57621L62.3444 0.0842285Z" fill="#F97B22"/>
-                    <path d="M88.0954 0.0842285L90.5297 7.57621L98.4072 7.57621L92.0342 12.2065L94.4685 19.6985L88.0954 15.0682L81.7224 19.6985L84.1567 12.2065L77.7836 7.57621L85.6611 7.57621L88.0954 0.0842285Z" fill="#C3C3C3"/>
-                    <path d="M113.846 0.0842285L116.281 7.57621L124.158 7.57621L117.785 12.2065L120.219 19.6985L113.846 15.0682L107.473 19.6985L109.908 12.2065L103.535 7.57621L111.412 7.57621L113.846 0.0842285Z" fill="#C3C3C3"/>
-                  </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="125"
+                  height="22"
+                  viewBox="0 0 125 22"
+                  fill="none"
+                >
+                  <path
+                    d="M10.8425 0.0842285L13.2768 7.57621L21.1543 7.57621L14.7813 12.2065L17.2155 19.6985L10.8425 15.0682L4.46943 19.6985L6.90373 12.2065L0.530669 7.57621L8.4082 7.57621L10.8425 0.0842285Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M36.5935 0.0842285L39.0278 7.57621L46.9053 7.57621L40.5322 12.2065L42.9665 19.6985L36.5935 15.0682L30.2204 19.6985L32.6547 12.2065L26.2816 7.57621L34.1592 7.57621L36.5935 0.0842285Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M62.3444 0.0842285L64.7787 7.57621L72.6563 7.57621L66.2832 12.2065L68.7175 19.6985L62.3444 15.0682L55.9714 19.6985L58.4057 12.2065L52.0326 7.57621L59.9102 7.57621L62.3444 0.0842285Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M88.0954 0.0842285L90.5297 7.57621L98.4072 7.57621L92.0342 12.2065L94.4685 19.6985L88.0954 15.0682L81.7224 19.6985L84.1567 12.2065L77.7836 7.57621L85.6611 7.57621L88.0954 0.0842285Z"
+                    fill="#C3C3C3"
+                  />
+                  <path
+                    d="M113.846 0.0842285L116.281 7.57621L124.158 7.57621L117.785 12.2065L120.219 19.6985L113.846 15.0682L107.473 19.6985L109.908 12.2065L103.535 7.57621L111.412 7.57621L113.846 0.0842285Z"
+                    fill="#C3C3C3"
+                  />
+                </svg>
               </div>
               <div>
                 <img src="../assets/images/reviews/4.jpg" alt="Cx" />
@@ -271,21 +319,42 @@
             </div>
             <div>
               <p class="p-2">
-           Best cleaning services that I’ve tried in while.
+                Best cleaning services that I’ve tried in while.
               </p>
             </div>
           </div>
-         <div class="box m-3 ">
+          <div class="box">
             <div class="d-flex flex-row justify-content-between">
               <div class="p-2">
                 <h4>Nada A.</h4>
-              <svg xmlns="http://www.w3.org/2000/svg" width="125" height="22" viewBox="0 0 125 22" fill="none">
-                    <path d="M10.8425 0.0842285L13.2768 7.57621L21.1543 7.57621L14.7813 12.2065L17.2155 19.6985L10.8425 15.0682L4.46943 19.6985L6.90373 12.2065L0.530669 7.57621L8.4082 7.57621L10.8425 0.0842285Z" fill="#F97B22"/>
-                    <path d="M36.5935 0.0842285L39.0278 7.57621L46.9053 7.57621L40.5322 12.2065L42.9665 19.6985L36.5935 15.0682L30.2204 19.6985L32.6547 12.2065L26.2816 7.57621L34.1592 7.57621L36.5935 0.0842285Z" fill="#F97B22"/>
-                    <path d="M62.3444 0.0842285L64.7787 7.57621L72.6563 7.57621L66.2832 12.2065L68.7175 19.6985L62.3444 15.0682L55.9714 19.6985L58.4057 12.2065L52.0326 7.57621L59.9102 7.57621L62.3444 0.0842285Z" fill="#F97B22"/>
-                    <path d="M88.0954 0.0842285L90.5297 7.57621L98.4072 7.57621L92.0342 12.2065L94.4685 19.6985L88.0954 15.0682L81.7224 19.6985L84.1567 12.2065L77.7836 7.57621L85.6611 7.57621L88.0954 0.0842285Z" fill="#F97B22"/>
-                    <path d="M113.846 0.0842285L116.281 7.57621L124.158 7.57621L117.785 12.2065L120.219 19.6985L113.846 15.0682L107.473 19.6985L109.908 12.2065L103.535 7.57621L111.412 7.57621L113.846 0.0842285Z" fill="#F97B22"/>
-                  </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="125"
+                  height="22"
+                  viewBox="0 0 125 22"
+                  fill="none"
+                >
+                  <path
+                    d="M10.8425 0.0842285L13.2768 7.57621L21.1543 7.57621L14.7813 12.2065L17.2155 19.6985L10.8425 15.0682L4.46943 19.6985L6.90373 12.2065L0.530669 7.57621L8.4082 7.57621L10.8425 0.0842285Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M36.5935 0.0842285L39.0278 7.57621L46.9053 7.57621L40.5322 12.2065L42.9665 19.6985L36.5935 15.0682L30.2204 19.6985L32.6547 12.2065L26.2816 7.57621L34.1592 7.57621L36.5935 0.0842285Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M62.3444 0.0842285L64.7787 7.57621L72.6563 7.57621L66.2832 12.2065L68.7175 19.6985L62.3444 15.0682L55.9714 19.6985L58.4057 12.2065L52.0326 7.57621L59.9102 7.57621L62.3444 0.0842285Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M88.0954 0.0842285L90.5297 7.57621L98.4072 7.57621L92.0342 12.2065L94.4685 19.6985L88.0954 15.0682L81.7224 19.6985L84.1567 12.2065L77.7836 7.57621L85.6611 7.57621L88.0954 0.0842285Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M113.846 0.0842285L116.281 7.57621L124.158 7.57621L117.785 12.2065L120.219 19.6985L113.846 15.0682L107.473 19.6985L109.908 12.2065L103.535 7.57621L111.412 7.57621L113.846 0.0842285Z"
+                    fill="#F97B22"
+                  />
+                </svg>
               </div>
               <div>
                 <img src="../assets/images/reviews/5.jpg" alt="Cx" />
@@ -293,20 +362,42 @@
             </div>
             <div>
               <p class="p-2">
-                I always count on Aown in babysitting my kids. I don’t have to worry when I’m at work.
+                I always count on Aown in babysitting my kids. I don’t have to
+                worry when I’m at work.
               </p>
             </div>
           </div>
-          <div class="box m-3 ">
+          <div class="box">
             <div class="d-flex flex-row justify-content-between">
               <div class="p-2">
                 <h4>Haidy M.</h4>
-               <svg xmlns="http://www.w3.org/2000/svg" width="125" height="22" viewBox="0 0 125 22" fill="none">
-                  <path d="M10.8425 0.0842438L13.2768 7.57622L21.1543 7.57622L14.7813 12.2065L17.2155 19.6985L10.8425 15.0682L4.46943 19.6985L6.90373 12.2065L0.530669 7.57622L8.4082 7.57622L10.8425 0.0842438Z" fill="#F97B22"/>
-                  <path d="M36.5935 0.0842438L39.0278 7.57622L46.9053 7.57622L40.5322 12.2065L42.9665 19.6985L36.5935 15.0682L30.2204 19.6985L32.6547 12.2065L26.2816 7.57622L34.1592 7.57622L36.5935 0.0842438Z" fill="#F97B22"/>
-                  <path d="M62.3444 0.0842438L64.7787 7.57622L72.6563 7.57622L66.2832 12.2065L68.7175 19.6985L62.3444 15.0682L55.9714 19.6985L58.4057 12.2065L52.0326 7.57622L59.9102 7.57622L62.3444 0.0842438Z" fill="#F97B22"/>
-                  <path d="M88.0954 0.0842438L90.5297 7.57622L98.4072 7.57622L92.0342 12.2065L94.4685 19.6985L88.0954 15.0682L81.7224 19.6985L84.1567 12.2065L77.7836 7.57622L85.6611 7.57622L88.0954 0.0842438Z" fill="#F97B22"/>
-                  <path d="M113.846 0.0842438L116.281 7.57622L124.158 7.57622L117.785 12.2065L120.219 19.6985L113.846 15.0682L107.473 19.6985L109.908 12.2065L103.535 7.57622L111.412 7.57622L113.846 0.0842438Z" fill="#F97B22"/>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="125"
+                  height="22"
+                  viewBox="0 0 125 22"
+                  fill="none"
+                >
+                  <path
+                    d="M10.8425 0.0842438L13.2768 7.57622L21.1543 7.57622L14.7813 12.2065L17.2155 19.6985L10.8425 15.0682L4.46943 19.6985L6.90373 12.2065L0.530669 7.57622L8.4082 7.57622L10.8425 0.0842438Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M36.5935 0.0842438L39.0278 7.57622L46.9053 7.57622L40.5322 12.2065L42.9665 19.6985L36.5935 15.0682L30.2204 19.6985L32.6547 12.2065L26.2816 7.57622L34.1592 7.57622L36.5935 0.0842438Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M62.3444 0.0842438L64.7787 7.57622L72.6563 7.57622L66.2832 12.2065L68.7175 19.6985L62.3444 15.0682L55.9714 19.6985L58.4057 12.2065L52.0326 7.57622L59.9102 7.57622L62.3444 0.0842438Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M88.0954 0.0842438L90.5297 7.57622L98.4072 7.57622L92.0342 12.2065L94.4685 19.6985L88.0954 15.0682L81.7224 19.6985L84.1567 12.2065L77.7836 7.57622L85.6611 7.57622L88.0954 0.0842438Z"
+                    fill="#F97B22"
+                  />
+                  <path
+                    d="M113.846 0.0842438L116.281 7.57622L124.158 7.57622L117.785 12.2065L120.219 19.6985L113.846 15.0682L107.473 19.6985L109.908 12.2065L103.535 7.57622L111.412 7.57622L113.846 0.0842438Z"
+                    fill="#F97B22"
+                  />
                 </svg>
               </div>
               <div>
@@ -314,13 +405,43 @@
               </div>
             </div>
             <div>
-              <p class="p-2">
-               very quick service.Love it!
-              </p>
+              <p class="p-2">very quick service.Love it!</p>
             </div>
           </div>
         </div>
       </div>
+      <div class="row text-center my-5">
+        <h2>Contact us</h2>
+      </div>
+      <div class="d-flex justify-content-center">
+        <form >
+                <div class="mb-3 col-lg-6 col-md-4">
+                    <div>
+                        <input type="text" class="input" id="inputName" placeholder="name">
+                    </div>
+                </div>
+                <div class="mb-3 col-lg-6 col-md-4">
+                    <div>
+                        <input type="email" class="input" id="inputEmail" placeholder="e-mail">
+                    </div>
+                </div>
+                <div class="mb-3 col-lg-6 col-md-4">
+                    <div>
+                        <input type="text" class="input" id="inputPhone" placeholder="phone number">
+                    </div>
+                </div>
+                <div class="mb-3 col-lg-6 col-md-4">
+                    <div>
+                        <textarea class="input" placeholder="write you message here" id="inputMessage"></textarea>
+                    </div>
+                </div>
+                <button class="btn d-block m-auto mb-3">send</button> 
+        </form>
+      
+      </div>
+    
+
+    
     </div>
     <FooterComponent />
   </div>
@@ -401,7 +522,7 @@ export default {
     width: 370px;
     padding: 15px;
     height: 190px;
-    margin: 0px;
+    margin: 20px;
 
     img {
       clip-path: circle();
@@ -411,5 +532,37 @@ export default {
   h4 {
     font-size: $paragraph;
   }
+
+  form {
+
+    .input{
+      border: 3px solid $blueColor;
+      border-radius: 33px;
+      width: 716px;
+      height: 69px;
+      padding: 9px 4px 9px 40px;
+      background: white;
+      @media (max-width: 767px) {
+        width: 350px;
+        height: 55px;
+      }
+      &:focus {
+        border-color: $orangeColor;
+        outline: none;
+      }
+      &#inputMessage{
+        height: 262px;
+      }
+   
+    }
+    .input[placeholder]{
+        font-size: $paragraph;
+        font-weight: $semiBold;
+    }
+    .btn {
+      @include button;
+    }
+  }
+  
 }
 </style>
