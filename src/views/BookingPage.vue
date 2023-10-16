967e1eb78272e4bf9965d227f5601a2ac04e3bec
NadaMohamed <nmimmohamed@gmail.com>
2023-10-16 17:41:10 +0200

    'updates

---
+++ src/views/BookingPage.vue
@@ -0,0 +1,53 @@
+<template>
+    <NavBarPages/>
+    
+    <div class="all">
+        <div class="container grid mb-5">
+            <div clas="row"><p class="title">Booking Details</p></div>
+
+            <div clas="row personlaInfo">
+
+            </div>
+
+
+
+
+
+
+        </div>
+
+
+    </div>
+    <FooterComponent/>
+</template>
+
+<script>
+import NavBarPages from '@/components/NavBarPages.vue';
+import FooterComponent from '../components/footer.vue'
+
+    export default {
+    name: 'BookingPage',
+    components: { 
+        NavBarPages ,
+        FooterComponent
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import '../scss/global/colors';
+@import '../scss/global/variables';
+
+.all{
+    background-color: $backgroundColor;
+    color:$blueColor
+}
+
+.title{
+    font-size: $title;
+    font-weight: $bold;
+}
+
+</style>
