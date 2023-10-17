25532dd6c8d55ecd1541081d99e7eef6e583f601
amrLmohamed <amr.199876@gmail.com>
2023-10-17 18:14:38 +0300

    save add

--- src/views/AddAgency.vue
+++ src/views/AddAgency.vue
@@ -7,7 +7,6 @@
         <h2 class="headline">Agency Info</h2>
       </div>
       <div>
-        <form class="">
           <div class="row m-4">
             <div class="col">
               <label >Full name</label>
@@ -68,7 +67,202 @@
               />
             </div>
           </div>
-        </form>
+          <div class="row m-4">
+            <div class="col">
+              <label for="">Front of tax card</label>
+              <input
+                type="file"
+                class="form-control w-100 rounded-5 "
+                placeholder="Front of tax card"
+                aria-label="Front of tax card"
+              />
+            </div>
+            <div class="col">
+              <label for="">Agency  logo</label>
+              <input
+                type="file"
+                class="form-control w-100 rounded-5"
+                placeholder="Agency  logo"
+                aria-label="Agency  logo"
+              />
+            </div>
+          </div>
+      </div>
+    </div>
+    <div class="">
+      <!-- <SideBarPage class="min-width"/> -->
+      <div class="d-flex border-2 justify-content-center flex-wrap">
+        <h2 class="headline">Agency Info</h2>
+      </div>
+      <div>
+          <div class="row m-4">
+            <div class="col">
+              <label >Full name</label>
+              <input
+                type="text"
+                class="form-control w-100 rounded-5"
+                placeholder="Full name"
+                aria-label="Full name"
+              />
+            </div>
+            <div class="col">
+              <label for="">Email</label>
+              <input
+                type="email"
+                class="form-control w-100 rounded-5"
+                placeholder="Email"
+                aria-label="Email"
+              />
+            </div>
+          </div>
+          <div class="row m-4">
+            <div class="col">
+              <label for="">Phone No1</label>
+              <input
+                type="text"
+                class="form-control w-100 rounded-5"
+                placeholder="Phone No1"
+                aria-label="Phone No1"
+              />
+            </div>
+            <div class="col">
+              <label for="">Phone No2</label>
+              <input
+                type="text"
+                class="form-control w-100 rounded-5"
+                placeholder="Phone No2"
+                aria-label="Phone No2"
+              />
+            </div>
+          </div>
+          <div class="row m-4">
+            <div class="col">
+              <label for="">Front of trade register card</label>
+              <input
+                type="file"
+                class="form-control w-100 rounded-5 "
+                placeholder="Front of trade register card"
+                aria-label="Front of trade register card"
+              />
+            </div>
+            <div class="col">
+              <label for="">Back of trade register card</label>
+              <input
+                type="file"
+                class="form-control w-100 rounded-5"
+                placeholder="Back of trade register card"
+                aria-label="Back of trade register card"
+              />
+            </div>
+          </div>
+          <div class="row m-4">
+            <div class="col">
+              <label for="">Front of tax card</label>
+              <input
+                type="file"
+                class="form-control w-100 rounded-5 "
+                placeholder="Front of tax card"
+                aria-label="Front of tax card"
+              />
+            </div>
+            <div class="col">
+              <label for="">Agency  logo</label>
+              <input
+                type="file"
+                class="form-control w-100 rounded-5"
+                placeholder="Agency  logo"
+                aria-label="Agency  logo"
+              />
+            </div>
+          </div>
+      </div>
+    </div>
+    <div class="">
+      <!-- <SideBarPage class="min-width"/> -->
+      <div class="d-flex border-2 justify-content-center flex-wrap">
+        <h2 class="headline">Agency Info</h2>
+      </div>
+      <div>
+          <div class="row m-4">
+            <div class="col">
+              <label >Full name</label>
+              <input
+                type="text"
+                class="form-control w-100 rounded-5"
+                placeholder="Full name"
+                aria-label="Full name"
+              />
+            </div>
+            <div class="col">
+              <label for="">Email</label>
+              <input
+                type="email"
+                class="form-control w-100 rounded-5"
+                placeholder="Email"
+                aria-label="Email"
+              />
+            </div>
+          </div>
+          <div class="row m-4">
+            <div class="col">
+              <label for="">Phone No1</label>
+              <input
+                type="text"
+                class="form-control w-100 rounded-5"
+                placeholder="Phone No1"
+                aria-label="Phone No1"
+              />
+            </div>
+            <div class="col">
+              <label for="">Phone No2</label>
+              <input
+                type="text"
+                class="form-control w-100 rounded-5"
+                placeholder="Phone No2"
+                aria-label="Phone No2"
+              />
+            </div>
+          </div>
+          <div class="row m-4">
+            <div class="col">
+              <label for="">Front of trade register card</label>
+              <input
+                type="file"
+                class="form-control w-100 rounded-5 "
+                placeholder="Front of trade register card"
+                aria-label="Front of trade register card"
+              />
+            </div>
+            <div class="col">
+              <label for="">Back of trade register card</label>
+              <input
+                type="file"
+                class="form-control w-100 rounded-5"
+                placeholder="Back of trade register card"
+                aria-label="Back of trade register card"
+              />
+            </div>
+          </div>
+          <div class="row m-4">
+            <div class="col">
+              <label for="">Front of tax card</label>
+              <input
+                type="file"
+                class="form-control w-100 rounded-5 "
+                placeholder="Front of tax card"
+                aria-label="Front of tax card"
+              />
+            </div>
+            <div class="col">
+              <label for="">Agency  logo</label>
+              <input
+                type="file"
+                class="form-control w-100 rounded-5"
+                placeholder="Agency  logo"
+                aria-label="Agency  logo"
+              />
+            </div>
+          </div>
       </div>
     </div>
   </div>
