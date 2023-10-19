6bea5333b2c5ccfb5a86f43ac4e3332358fd491b
NadaMohamed <nmimmohamed@gmail.com>
2023-10-19 22:56:11 +0200

    'update'

--- src/router/index.js
+++ src/router/index.js
@@ -12,21 +12,8 @@ import AgancyDash from "../views/AgancyDash.vue";
 import WorkerPageDashboard from "../views/workerPageDashboard.vue";
 import dashReviews from "../views/dashReviews.vue";
 import aboutus from "../views/About.vue";
-import allAgencies from "../views/allAgencySDash.vue";
-import Allworker from "../views/AllWorker.vue";
-
-import CleaningView from "../views/CleaningView.vue";
-import GardeningView from "../views/GardeningView.vue";
-import BabysittingView from "../views/BabysittingView.vue";
-import ElderlycareView from "../views/ElderlycareView.vue";
-import HousenursingView from "../views/HousenursingView.vue";
-import ChefsView from "../views/ChefsView.vue";
-import FoodcateringView from "../views/FoodcateringView.vue";
-import SADashHome from "../views/SADashHome.vue"
-import AllWorkerFromCX from "../views/AllWorkerFromCX.vue"
-// >>>>>>> 7daaeeb6701d2e0c36ee0a5e20989dc98c5f82d5
-import AddAgency from "../views/AddAgency.vue";
-
+import Allworker from "../views/AllWorker.vue"
+import ServiceHistoryAgency from "../views/ServiceHistoryAgency.vue";
 // start of auth
 // user auth
 import LoginView from "../views/auth/user_auth/LoginView.vue";
@@ -128,10 +115,7 @@ const routes = [
     component: ServicesView,
   },
   {
-    path:  "/workerprofileDash",
-    name:  "WorkerPageDashboard",
-    component:  WorkerPageDashboard,
-    path: "/workerprofileDash",
+    path: "/workerpageDash",
     name: "WorkerPageDashboard",
     component: WorkerPageDashboard,
   },
@@ -173,115 +157,17 @@ const routes = [
   {
     path: "/ServiceHistorySA",
     name: "ServiceHistorySA",
-    component: ServiceHistorySA,
-  },
-  {
-    path: "/CleaningView",
-    name: "CleaningView",
-    component: CleaningView,
-  },
-  {
-    path: "/GardeningView",
-    name: "GardeningView",
-    component: GardeningView,
-  },
-  {
-    path: "/BabysittingView",
-    name: "BabysittingView",
-    component: BabysittingView,
-  },
-  {
-    path: "/ElderlycareView",
-    name: "ElderlycareView",
-    component: ElderlycareView,
-  },
-  {
-    path: "/HousenursingView",
-    name: "HousenursingView",
-    component: HousenursingView,
-  },
-  {
-    path: "/ChefsView",
-    name: "ChefsView",
-    component: ChefsView,
-  },
-  {
-    path: "/FoodcateringView",
-    name: "FoodcateringView",
-    component: FoodcateringView,
-  },
-// =======
-  {
-    path: "/ServiceHistorySA",
-    name: "ServiceHistorySA",
-    component: ServiceHistorySA,
-  },
-  {
-    path: "/CleaningView",
-    name: "CleaningView",
-    component: CleaningView,
-  },
-  {
-    path: "/GardeningView",
-    name: "GardeningView",
-    component: GardeningView,
-  },
-  {
-    path: "/BabysittingView",
-    name: "BabysittingView",
-    component: BabysittingView,
-  },
-  {
-    path: "/ElderlycareView",
-    name: "ElderlycareView",
-    component: ElderlycareView,
-  },
-  {
-    path: "/HousenursingView",
-    name: "HousenursingView",
-    component: HousenursingView,
-  },
-  {
-    path: "/ChefsView",
-    name: "ChefsView",
-    component: ChefsView,
-  },
-  {
-    path: "/FoodcateringView",
-    name: "FoodcateringView",
-    component: FoodcateringView,
-  },
-// =======
-  {
-    path: "/serviceHistorySA",
-    name: "ServiceHistorySA",
-    component: ServiceHistorySA,
+    component: ServiceHistorySA
   },
   {
     path: "/booking",
     name: "BookingPage",
-    component: BookingPage,
+    component: BookingPage
   },
   {
-    path: "/AddAgency",
-    name: "AddAgency",
-    component: AddAgency,
-  },
-  {
-    path: "/AddAgency",
-    name: "AddAgency",
-    component: AddAgency,
-  },
-
-  {
-    path: "/SAdashhome",
-    name: "SADashHome",
-    component: SADashHome,
-  },
-  {
-    path: "/allworkerfromcx",
-    name: "AllWorkerFromCX",
-    component: AllWorkerFromCX,
+    path: "/ServiceHistoryAgency",
+    name: "ServiceHistoryAgency",
+    component: ServiceHistoryAgency
   },
 
 ];
