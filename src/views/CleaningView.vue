b43588242940f3d62ad924af81ce887c893101b2
omar al farouk <omarelfarouk.tarek@gmail.com>
2023-10-26 05:06:01 +0300

    errors

--- src/views/CleaningView.vue
+++ src/views/CleaningView.vue
@@ -1,63 +1,27 @@
 <template>
   <div class="bg container-fluid">
     <NavBarPages />
-    <div class="bar d-flex justify-content-center">
-      <form class="search">
-        <input class="search" type="search" />
+    <div class="child d-flex justify-content-center">
+      <form>
+        <div class="mb-3 col-lg-6 col-md-4">
+          <div>
+            <input type="text" class="input" id="inputName" placeholder="Bedroom, Living room" />
+          </div>
+        </div>
+        <div class="mb-3 col-lg-6 col-md-4">
+          <div>
+            <input type="email" class="input" id="inputEmail" placeholder="Bathroom" />
+          </div>
+        </div>
+        <div class="mb-3 col-lg-6 col-md-4">
+          <div>
+            <input type="text" class="input" id="inputPhone" placeholder="Kitchen" />
+          </div>
+        </div>
+        <div class="mb-3 col-lg-6 col-md-4"></div>
       </form>
     </div>
-    <div class="d-flex justify-content-center">
-      <nav class="accordion arrows">
-        <input type="radio" name="accordion" id="cb1" />
-        <section class="box">
-          <label class="box-title" for="cb1">Bedroom , Living room</label>
-          <label class="box-close" for="acc-close"></label>
-          <div class="box-content">
-            <ul>
-              <li>Dust all accessible surfaces</li>
-              <li>Wipe down all mirrors and glass fixtures</li>
-              <li>Clean all floor surfaces</li>
-              <li>Take out garbage and recycling</li>
-              <li>Take out trash</li>
-              <li>Wipe down windows and mirrors</li>
-            </ul>
-          </div>
-        </section>
-        <input type="radio" name="accordion" id="cb2" />
-        <section class="box">
-          <label class="box-title" for="cb2">Bathroom</label>
-          <label class="box-close" for="acc-close"></label>
-          <div class="box-content">
-            <ul>
-              <li>Wash and sanitize the toilet, shower, tub and sink</li>
-              <li>Dust all accessible surfaces</li>
-              <li>Wipe down all mirrors and glass fixtures</li>
-              <li>Clean all floor surfaces</li>
-              <li>Take out garbage and recycling</li>
-            </ul>
-          </div>
-        </section>
-        <input type="radio" name="accordion" id="cb3" />
-        <section class="box mb-5">
-          <label class="box-title" for="cb3"
-            >Kitchen</label
-          >
-          <label class="box-close" for="acc-close"></label>
-          <div class="box-content">
-            <ul>
-              <li>Dust all accessible surfaces</li>
-              <li>Empty sink and load up dishwasher with dirty dishes</li>
-              <li>Wipe down exterior of stove, oven and fridge</li>
-              <li>Clean all floor surfaces</li>
-              <li>Take out garbage and recycling </li>
-            </ul>
-          </div>
-        </section>
-
-        <input type="radio" name="accordion" id="acc-close" />
-      </nav>
-    </div>
-    <div class="row justify-content-center my-5 gap-5">
+    <!-- <div class="row justify-content-center my-5 gap-5">
       <div class="card mb-4" style="width: 18rem">
         <img
           src="../assets/subservices/cleaner3.jpg"
@@ -431,6 +395,38 @@
           <a href="#" class="btn d-block m-auto mb-3 mt-3">Book Now</a>
         </div>
       </div>
+    </div> -->
+    <!-- http://localhost:8080/img/cleaner1.65740dc5.jpg -->
+    <!-- <img src="@/assets/subservices/cleaner1.jpg" alt=""> -->
+    <div class="container">
+      <div class="row gy-4">
+        <div class="card col-md-4 col-sm-6 col-6" v-for="(worker, i) in workerData" :key="i">
+          <img :src="${worker.img}" class="card-img-top object-fit-cover" :alt="workerimg${i}" />
+          <div class="card-body">
+            <h5 class="card-title">{{ worker['First_name'] }} {{ worker['Last_name'] }}</h5>
+            <p class="card-text">16 St. Emad Eldein - Down Town - Cairo</p>
+            <svg class="d-block m-auto" xmlns="http://www.w3.org/2000/svg" width="125" height="22" viewBox="0 0 125 22"
+              fill="none">
+              <path
+                d="M10.8425 0.0842438L13.2768 7.57622L21.1543 7.57622L14.7813 12.2065L17.2155 19.6985L10.8425 15.0682L4.46943 19.6985L6.90373 12.2065L0.530669 7.57622L8.4082 7.57622L10.8425 0.0842438Z"
+                fill="#F97B22" />
+              <path
+                d="M36.5935 0.0842438L39.0278 7.57622L46.9053 7.57622L40.5322 12.2065L42.9665 19.6985L36.5935 15.0682L30.2204 19.6985L32.6547 12.2065L26.2816 7.57622L34.1592 7.57622L36.5935 0.0842438Z"
+                fill="#F97B22" />
+              <path
+                d="M62.3444 0.0842438L64.7787 7.57622L72.6563 7.57622L66.2832 12.2065L68.7175 19.6985L62.3444 15.0682L55.9714 19.6985L58.4057 12.2065L52.0326 7.57622L59.9102 7.57622L62.3444 0.0842438Z"
+                fill="#F97B22" />
+              <path
+                d="M88.0954 0.0842438L90.5297 7.57622L98.4072 7.57622L92.0342 12.2065L94.4685 19.6985L88.0954 15.0682L81.7224 19.6985L84.1567 12.2065L77.7836 7.57622L85.6611 7.57622L88.0954 0.0842438Z"
+                fill="#F97B22" />
+              <path
+                d="M113.846 0.0842438L116.281 7.57622L124.158 7.57622L117.785 12.2065L120.219 19.6985L113.846 15.0682L107.473 19.6985L109.908 12.2065L103.535 7.57622L111.412 7.57622L113.846 0.0842438Z"
+                fill="#F97B22" />
+            </svg>
+            <a href="#" class="btn d-block m-auto mb-3 mt-3">Book Now</a>
+          </div>
+        </div>
+      </div>
     </div>
     <FooterComponent />
   </div>
@@ -439,11 +435,26 @@
 <script>
 import NavBarPages from "@/components/NavBarPages.vue";
 import FooterComponent from "@/components/footer.vue";
+import axios from "axios";
 export default {
   components: {
     NavBarPages,
     FooterComponent,
   },
+  data() {
+    return {
+      // pathImg: "../assets/subservices/",
+      // imgArr:[],
+      workerData: [],
+    }
+  },
+  created() {
+    axios.get("http://localhost:2000/worker").then((res) => {
+      this.workerData = res.data;
+      // console.log(res.data);
+      // this.pathImg = "../assets/subservices/" + res.data['img'];
+    }).catch(err => console.log(err))
+  }
 };
 </script>
 
@@ -455,28 +466,7 @@ export default {
 .bg {
   background-color: $backgroundColor;
 }
-.bar {
-  justify-content: center;
-  width: auto;
-  height: 200px;
 
-}
-input.search {
-  border: 2px solid $orangeColor;
-  border-radius: 28px;
-  width: 350px;
-  padding: 9px 4px 9px 40px;
-  // margin-left: 550px;
-  margin-top: 75px;
-  margin-bottom: 50px;
-  background: $backgroundColor
-    url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='orange' width='16' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E")
-    no-repeat 13px center;
-  &:focus {
-    border-color: $orangeColor;
-    outline: none;
-  }
-}
 form {
   .input {
     border: 3px solid $blueColor;
@@ -485,23 +475,28 @@ form {
     height: 69px;
     padding: 9px 4px 9px 40px;
     background: white;
+
     @media (max-width: 767px) {
       width: 350px;
       height: 55px;
     }
+
     &:focus {
       border-color: $orangeColor;
       outline: none;
     }
+
     &#inputMessage {
       height: 262px;
     }
   }
+
   .input[placeholder] {
     font-size: $paragraph;
     font-weight: $semiBold;
   }
 }
+
 .child {
   margin: auto;
   margin-top: 50px;
@@ -512,6 +507,7 @@ form {
   height: auto;
   //   border-radius: $border-radius-big;
 }
+
 .serv {
   font-size: $title;
   font-family: "raleway";
@@ -532,6 +528,9 @@ img {
   height: 200px;
 }
 
+.card{
+  // width: 270px;
+}
 .card-title {
   text-align: center;
   font-size: $subTitle;
@@ -544,87 +543,8 @@ img {
   font-size: $paragraph;
   color: $blueColor;
 }
+
 .btn {
   @include button;
-}
-
-.accordion {
-  margin: auto;
-  // width: 50%;
-}
-.accordion input {
-  display: none;
-}
-.accordion {
-  .box {
-    position: relative;
-    background: white;
-    height: 64px;
-    transition: all 1s linear;
-    border: 3px solid $blueColor;
-    border-radius: 33px;
-    width: 716px;
-    height: 69px;
-    padding: 9px 4px 9px 40px;
-    margin-bottom: 10px;
-    background: white;
-    @media (max-width: 767px) {
-      width: 350px;
-      height: 55px;
-    }
-  }
-
-  .box-title {
-    font-weight: $semiBold;
-    font-size: $small;
-    color: $blueColor;
-    width: calc(100% - 30px);
-    height: 64px;
-    text-align: center;
-    line-height: 36px;
-    display: inline-block;
-    cursor: pointer;
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-
-    @media (min-width: 767px) {
-      font-size: 20px;
-    }
-  }
-  .box-content {
-    width: calc(100% - 40px);
-    padding: 30px 20px;
-    font-size: $small;
-    font-weight: $semiBold;
-    color: black;
-    display: none;
-    @media (min-width: 767px) {
-      font-size: 20px;
-    }
-  }
-  .box-close {
-    position: absolute;
-    height: 64px;
-    width: 100%;
-    top: 0;
-    left: 0;
-    cursor: pointer;
-    display: none;
-  }
-  input:checked + .box {
-    height: auto;
-    margin-top: 20px;
-  }
-  input:checked + .box .box-title {
-    border-bottom: 1px solid $blueColor;
-  }
-  input:checked + .box .box-content,
-  input:checked + .box .box-close {
-    display: inline-block;
-  }
 }
 </style>
