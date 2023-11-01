8edf580861a587304f3dafdff4c256d5b318af45
amrLmohamed <amr.199876@gmail.com>
2023-11-01 06:27:53 +0200

    last commit

--- src/views/AddAgency.vue
+++ src/views/AddAgency.vue
@@ -1,215 +1,219 @@
 <template>
   <div class="page">
     <NavBarSADash />
-    <form action="" class="form-control">
+    <form @submit.prevent="handlSubmit()" class="form-control">
       <div class="d-flex border-2 justify-content-center flex-wrap">
         <h2 class="headline">Agency Info</h2>
       </div>
       <div>
-          <div class="row m-4">
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label >Full name</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5"
-                placeholder="Full name"
-                aria-label="Full name"
-                v-model="FormValue.Agency_Name"
-              />
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label for="">Email</label>
-              <input
-                type="email"
-                class="form-control w-100 rounded-5"
-                placeholder="Email"
-                aria-label="Email"
-                v-model="FormValue.Agency_Email"
-              />
-            </div>
+        <div class="row m-4">
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label>Full name</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="Full name"
+              aria-label="Full name"
+              v-model="FormValue.Agency_Name"
+            />
           </div>
-          <div class="row m-4">
-            <div class="col-lg- col-md-6 col-sm-12">
-              <label for="">Phone No1</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5"
-                placeholder="Phone No1"
-                aria-label="Phone No1"
-                v-model="FormValue.Agency_Phone_no"
-              />
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label for="">Phone No2</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5"
-                placeholder="Phone No2"
-                aria-label="Phone No2"
-                v-model="FormValue.Agency_Phone_no2"
-              />
-            </div>
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label for="">Email</label>
+            <input
+              type="email"
+              class="form-control w-100 rounded-5"
+              placeholder="Email"
+              aria-label="Email"
+              v-model="FormValue.Email"
+            />
           </div>
-          <div class="row m-4">
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label for="">Front of trade register card</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5 "
-                placeholder="Front of trade register card"
-                aria-label="Front of trade register card"
-                v-model="FormValue.trade_register_card"
-              />
-            </div>
-            <div class="col-6 col-md-6 col-sm-12">
-              <label for="">Agency  logo</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5"
-                placeholder="Agency  logo"
-                aria-label="Agency  logo"
-                v-model="FormValue.Logo"
-              />
-            </div>
+        </div>
+        <div class="row m-4">
+          <div class="col-lg- col-md-6 col-sm-12">
+            <label for="">Phone No1</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="Phone No1"
+              aria-label="Phone No1"
+              v-model="FormValue.Agency_Phone_no"
+            />
           </div>
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label for="">Phone No2</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="Phone No2"
+              aria-label="Phone No2"
+              v-model="FormValue.Agency_Phone_no2"
+            />
+          </div>
+        </div>
+        <div class="row m-4">
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label for="">Front of trade register card</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="Front of trade register card"
+              aria-label="Front of trade register card"
+              v-model="FormValue.trade_register_card"
+            />
+          </div>
+          <div class="col-6 col-md-6 col-sm-12">
+            <label for="">Agency logo</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="Agency  logo"
+              aria-label="Agency  logo"
+              v-model="FormValue.Logo"
+            />
+          </div>
+        </div>
       </div>
       <div class="d-flex border-2 justify-content-center flex-wrap">
         <h2 class="headline">Owner Details</h2>
       </div>
       <div>
-          <div class="row m-4">
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label>First name</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5"
-                placeholder="First name"
-                aria-label="First name"
-                v-model="FormValue.Owner_First_name"
-              />
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label for="">Last name</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5"
-                placeholder="Last name"
-                aria-label="Last name"
-                v-model="FormValue.Owner_last_name"
-              />
-            </div>
+        <div class="row m-4">
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label>First name</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="First name"
+              aria-label="First name"
+              v-model="FormValue.Owner_First_name"
+            />
           </div>
-          <div class="row m-4">
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label for="">Phone No.</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5"
-                placeholder="Phone No."
-                aria-label="Phone No."
-                v-model="FormValue.Owner_Phone_no"
-              />
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label for="">Date of Birth</label>
-              <input
-                type="date"
-                class="form-control w-100 rounded-5"
-                placeholder="Date of Birth"
-                aria-label="Date of Birth"
-                v-model="FormValue.Owner_DOB"
-              />
-            </div>
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label for="">Last name</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="Last name"
+              aria-label="Last name"
+              v-model="FormValue.Owner_last_name"
+            />
           </div>
-          <div class="row m-4">
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label>Front of trade register card</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5 "
-                placeholder="Front of trade register card"
-                aria-label="Front of trade register card"
-                v-model="FormValue.Owner_front_nid"
-              />
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label for="">Back of trade register card</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5"
-                placeholder="Back of trade register card"
-                aria-label="Back of trade register card"
-                v-model="FormValue.Owner_back_nid"
-              />
-            </div>
+        </div>
+        <div class="row m-4">
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label for="">Phone No.</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="Phone No."
+              aria-label="Phone No."
+              v-model="FormValue.Owner_Phone_no"
+            />
           </div>
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label for="">Date of Birth</label>
+            <input
+              type="date"
+              class="form-control w-100 rounded-5"
+              placeholder="Date of Birth"
+              aria-label="Date of Birth"
+              v-model="FormValue.Owner_DOB"
+            />
+          </div>
+        </div>
+        <div class="row m-4">
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label>Front of trade register card</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="Front of trade register card"
+              aria-label="Front of trade register card"
+              v-model="FormValue.Owner_front_nid"
+            />
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label for="">Back of trade register card</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="Back of trade register card"
+              aria-label="Back of trade register card"
+              v-model="FormValue.Owner_back_nid"
+            />
+          </div>
+        </div>
       </div>
       <div class="d-flex border-2 justify-content-center flex-wrap">
         <h2 class="headline">Agency location</h2>
       </div>
       <div>
-          <div class="row m-4">
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label >Country</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5"
-                placeholder="Country"
-                aria-label="Country"
-              />
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label for="">governorate</label>
-              <input
-                type="email"
-                class="form-control w-100 rounded-5"
-                placeholder="governorate"
-                aria-label="governorate"
-              />
-            </div>
+        <div class="row m-4">
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label>Country</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="Country"
+              aria-label="Country"
+            />
           </div>
-          <div class="row m-4">
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label for="">City</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5"
-                placeholder="City"
-                aria-label="City"
-              />
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-              <label for="">Area</label>
-              <input
-                type="text"
-                class="form-control w-100 rounded-5"
-                placeholder="Area"
-                aria-label="Area"
-              />
-            </div>
-          </div>
-          <div class="row m-4">
-            <div class="col-12">
-              <label for="">Full Address</label>
-              <input
-                type="file"
-                class="form-control w-100 rounded-5 "
-                placeholder="Full Address"
-                aria-label="Full Address"
-              />
-            </div>
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label for="">governorate</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="governorate"
+              aria-label="governorate"
+              v-model="FormValue.Agency_Governorate"
+            />
           </div>
         </div>
-        <div class="row m-4 ">
-            <div class="col-lg-8 col-md-4 col-sm-12">
-              <input type="radio">
-              <label for="" >agree on terms and conditions</label>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-12 ">
-              <button>Add agency</button>
+        <div class="row m-4">
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label for="">City</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="City"
+              aria-label="City"
+              v-model="FormValue.Agency_City"
+            />
           </div>
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label for="">Area</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="Area"
+              aria-label="Area"
+              v-model="FormValue.Agency_Area"
+            />
+          </div>
+        </div>
+        <div class="row m-4">
+          <div class="col-12">
+            <label for="">Full Address</label>
+            <input
+              type="text"
+              class="form-control w-100 rounded-5"
+              placeholder="Full Address"
+              aria-label="Full Address"
+              v-model="FormValue.Agency_Address"
+            />
+          </div>
+        </div>
       </div>
-  </form>
+      <div class="row m-4">
+        <div class="col-lg-8 col-md-4 col-sm-12">
+          <input type="radio" />
+          <label for="">agree on terms and conditions</label>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <button>Add agency</button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -221,32 +225,37 @@ export default {
   components: {
     NavBarSADash,
   },
-  data(){
-    return{
-      FormValue:{
-        id:"",
-        Owner_First_name:"",
-        Owner_last_name:"",
-        Email:"",
-        Gender:"",
-        Owner_Phone_no:"",
-        Owner_DOB:"",
-        Owner_front_nid:"https://id.nadra.gov.pk/wp-content/uploads/2023/09/NICOP-2.png",
-        Owner_back_nid:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlGSAy9H4xvsdU501DmqFiYlrVc8YgZQ9bIQ&usqp=CAU",
-        trade_register_card:"https://cdn-fhdpe.nitrocdn.com/BLYhuUokNZUPConOXRxXZciCtaTwvpft/assets/static/optimized/rev-3abaf38/wp-content/uploads/2021/01/BCL-BRN-763x1024.jpg",
-        rating:"",
-        Logo:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAEoSURBVDjLrZMxSgRBEEXfiBiIkRjpRTyAuZGH8ALewmtsbOAVNjM1F0FEwURwZ9vq+t+gZ3aR3RFEC5qGLv6v11XdnW3+ErsAs9nMpRT6vme5XLJYLDZW3/erfCmF+XzeAXT/QgBwc7ewASUYsOHidL+7vn1eVzCkzNX5cbdhkIKjgx0EWPDyrpXu5HAP2Ujw+LrcTmBDuu0y1LV+JVaaSG83qAnS2iBzPDdZQTKZIsqEgQYC6TtBraKmUJoqUaL+TNAMjLI5RIhaW/VMTxNUtUbKbgTDFT7DK4pMU8bExhRSpLp1DzwaJFFFDhQRUwSGp7ckh4mM7ytKUqNVrzIREwSXZwfdtpdWwsRQXWpT2WowFRHZxNl6I+l3BvXT3D98bAjH+PNn+gIL+yQjrYYUIQAAAABJRU5ErkJggg==",
-        Agency_Name:"",
-        Agency_Email:"",
-        Agency_Address:"",
-        Agency_PostCode:"",
-        Agency_City:"",
-        Agency_Governorate:"",
-        Agency_Phone_no:"",
-        Agency_Phone_no2:"",
-        owner_Password:"",
-      }
-    }
+  data() {
+    return {
+      FormValue: {
+        id: "",
+        Owner_First_name: "",
+        Owner_last_name: "",
+        Email: "",
+        Gender: "",
+        Owner_Phone_no: "",
+        Owner_DOB: "",
+        Owner_front_nid:
+          "https://id.nadra.gov.pk/wp-content/uploads/2023/09/NICOP-2.png",
+        Owner_back_nid:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlGSAy9H4xvsdU501DmqFiYlrVc8YgZQ9bIQ&usqp=CAU",
+        trade_register_card:
+          "https://cdn-fhdpe.nitrocdn.com/BLYhuUokNZUPConOXRxXZciCtaTwvpft/assets/static/optimized/rev-3abaf38/wp-content/uploads/2021/01/BCL-BRN-763x1024.jpg",
+        rating: "",
+        Logo: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAEoSURBVDjLrZMxSgRBEEXfiBiIkRjpRTyAuZGH8ALewmtsbOAVNjM1F0FEwURwZ9vq+t+gZ3aR3RFEC5qGLv6v11XdnW3+ErsAs9nMpRT6vme5XLJYLDZW3/erfCmF+XzeAXT/QgBwc7ewASUYsOHidL+7vn1eVzCkzNX5cbdhkIKjgx0EWPDyrpXu5HAP2Ujw+LrcTmBDuu0y1LV+JVaaSG83qAnS2iBzPDdZQTKZIsqEgQYC6TtBraKmUJoqUaL+TNAMjLI5RIhaW/VMTxNUtUbKbgTDFT7DK4pMU8bExhRSpLp1DzwaJFFFDhQRUwSGp7ckh4mM7ytKUqNVrzIREwSXZwfdtpdWwsRQXWpT2WowFRHZxNl6I+l3BvXT3D98bAjH+PNn+gIL+yQjrYYUIQAAAABJRU5ErkJggg==",
+        Agency_Name: "",
+        Agency_Email: "",
+        Agency_Address: "",
+        Agency_PostCode: "",
+        Agency_City: "",
+        Agency_Governorate: "",
+        Agency_Phone_no: "",
+        Agency_Phone_no2: "",
+        owner_Password: "",
+        Agency_Country:"",
+        Agency_Area:"",
+      },
+    };
   },
   methods: {
     handlSubmit() {
@@ -255,6 +264,7 @@ export default {
         .then((res) => console.log(res))
         .catch((err) => console.log(err));
       this.$router.push("/allAgencies");
+      
     },
   },
 };
@@ -270,8 +280,8 @@ export default {
   font: $paragraph;
   color: $blueColor;
 }
-button{
-  @include button
+button {
+  @include button;
 }
 .headline {
   display: flex;
@@ -283,12 +293,12 @@ button{
   width: 90%;
   margin-top: 20px;
 }
-.form-control{
+.form-control {
   background-color: $backgroundColor;
   label {
     color: $blueColor;
   }
-  input{
+  input {
     background-color: white;
   }
 }
--- src/views/SAServiceCategories.vue
+++ src/views/SAServiceCategories.vue
@@ -5,13 +5,18 @@
       <div class="row pt-3 breadCrumb">
         <ol class="breadcrumb d-flex col-6">
           <li class="breadcrumb-item me-2">Dashboard</li>
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512" fill="#F97B22" class="mt-2 me-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 320 512"
+            fill="#F97B22"
+            class="mt-2 me-2"
+          >
             <path
-              d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
+              d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"
+            />
           </svg>
-          <li class="breadcrumb-item" aria-current="page">
-            Services
-          </li>
+          <li class="breadcrumb-item" aria-current="page">Services</li>
         </ol>
         <ol class="addCard mx-2 col-lg-10 col-md-12 col-sm-12">
           <li>
@@ -22,15 +27,92 @@
         </ol>
       </div>
       <div class="row justify-content-center mt-3">
-        <div class="col-12 col-lg-8 box" v-for="(service, id) in services" :key="id">
+        <div
+          class="col-12 col-lg-8 box"
+          v-for="(service, id) in services"
+          :key="id"
+        >
           <div class="card my-3">
-            <img :src="`${service.img}`" class="card-img-top Clip-path:circle()" alt="..." />
+            <img
+              :src="`${service.img}`"
+              class="card-img-top Clip-path:circle()"
+              alt="..."
+            />
             <div class="card-body">
               <h5 class="card-title">{{ service["Name"] }}</h5>
               <p class="m-3">{{ service["Details"] }}</p>
               <div class="d-flex justify-content-end align-item-end">
-                <button class="btn" @click="deleteCat(service.id)">Remove service</button>
-                <button class="btn">Edit Service</button>
+                <button class="btn" @click="deleteCat(service.id)">
+                  Remove service
+                </button>
+                <routre-link to="/#">
+                  <button class="btn"  @click="EditData">Edit Service</button>
+                </routre-link>
+              </div>
+              <div
+                class="modal fade"
+                id="exampleModalToggle"
+                aria-hidden="true"
+                aria-labelledby="exampleModalToggleLabel"
+                tabindex="-1"
+              >
+                <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1
+                        class="modal-title fs-5 fw-bold"
+                        id="exampleModalToggleLabel"
+                      >
+                        Edit Service
+                      </h1>
+                      <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div class="modal-body">
+                      <div class="row mb-3">
+                        <div class="col-4">Agency Name</div>
+                        <div class="col-8">
+                          <input
+                            type="text"
+                            placeholder=""
+                            v-model="service.Name"
+                          />
+                        </div>
+                      </div>
+
+                      <div class="row mb-3">
+                        <div class="col-4">Details</div>
+                        <div class="col-8">
+                          <input
+                            type="text"
+                            placeholder=""
+                            v-model="service.Details"
+                          />
+                        </div>
+                      </div>
+
+                      <div class="row mb-3">
+                        <div class="col-4">Category</div>
+                        <div class="col-8">
+                          <input type="text" v-model="service.Category_id" />
+                        </div>
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button
+                        class="btn editBtn"
+                        data-bs-dismiss="modal"
+                        @click="EditData"
+                      >
+                        Edit Service
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -50,7 +132,13 @@ export default {
   },
   data() {
     return {
-      services: [],
+      services: {
+        Category_id: "",
+        id: "",
+        Name: "",
+        Details: "",
+        img: "https://i.postimg.cc/hjHcRHDm/cleaning.jpg",
+      },
     };
   },
   created() {
@@ -62,19 +150,36 @@ export default {
         .get("http://localhost:2000/service")
         .then((res) => {
           this.services = res.data;
+          this.services.Category_id = res.data.Category_id;
+          this.services.id = res.data.id;
+          this.services.Name = res.data.Name;
+          this.services.Details = res.data.Details;
+          this.services.img = res.data.img;
         })
         .catch((err) => {
           console.log(err);
         });
     },
     deleteCat(id) {
-      axios.delete(`http://localhost:2000/service/${id}`)
+      axios
+        .delete(`http://localhost:2000/service/${id}`)
         .then((res) => console.log(res))
         .catch((err) => {
           console.log(err);
         });
-      this.getServices()
-    }
+      this.getServices();
+    },
+    EditData() {
+      axios
+        .put(`http://localhost:2000/service/${this.id}`, this.services)
+        .then(() => {
+          this.getServices();
+        })
+        .catch((err) => console.log(err))
+        .then(this.$router.push("/SAServiceCategories"));
+      this.getServices();
+      // location.reload()
+    },
   },
 };
 </script>
